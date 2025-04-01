6f3fc78e106ca0dddee681e08ef58dc288b4a1f0
Daniel Pamich <daniel@silverdawnsoftware.com>
2025-04-01 14:55:54 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 IMAGE=glove80-zmk-config-docker-rgb
-BRANCH="${1:-main}"
+BRANCH="${1:-rgb-layer-24.12}"
 
 docker build -t "$IMAGE" .
 docker run --rm -v "$PWD:/config" -e UID="$(id -u)" -e GID="$(id -g)" -e BRANCH="$BRANCH" "$IMAGE"
